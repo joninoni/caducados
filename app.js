17cf35e1992bac21b7f2f59dac9b671807008883
joninoni <jonibeltranhayer@gmail.com>
2023-12-13 23:10:01 -0600

    creacion de varibles que son los inputs

---
+++ app.js
@@ -0,0 +1,10 @@
+// Variables
+const producto=document.querySelector("#producto");
+const codigo=document.querySelector("#codigo");
+const cantidad=document.querySelector("#cantidad");
+const fecha=document.querySelector("#fecha");
+const btnEnviar=document.querySelector(`input[type="submit"]`);
+const lista=document.querySelector("#lista");
+// Eventos
+
+// Funciones
