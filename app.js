2f0de6029e65e017003f7f362515e7f9076c4624
joninoni <jonibeltranhayer@gmail.com>
2023-12-15 13:08:27 -0600

    borrando todo dejando solo las variables y los eventos

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const cantidad=document.querySelector("#cantidad");
 const fecha=document.querySelector("#fecha");
 const btnEnviar=document.querySelector(`input[type="submit"]`);
 const lista=document.querySelector("#lista");
-let productosCaducados=[];
 // Eventos
 producto.addEventListener("blur",validarEmail);
 codigo.addEventListener("blur",validarEmail);
@@ -18,28 +17,7 @@ function validarEmail(e){
     if(e.target.value.trim()===""){
         console.log("no puede haber cmpos vacios");
         return
-    }
-    const caducados={
-        producto:"",
-        codigo:"",
-        cantidad:"",
-        fecha:"",
-    }
-    console.log(caducados);
-    productosCaducados=[...productosCaducados,caducados];
-    console.log(productosCaducados);
-    comprobarFormulario(caducados);
+    }   
 }
-function comprobarFormulario(caducados){
-    if(Object.values(caducados).includes("")){
-        btnEnviar.disabled=true;
-        return;
-    }
-    btnEnviar.disabled=false;
-}
-function crearHtml(e){
-    e.preventDefault();
-    const parrafo=document.createElement('p');
-    parrafo.textContent=`${caducados.producto} ${caducados.codigo} ${caducados.cantidad} ${caducados.fecha}`;
-    lista.appendChild(parrafo);
-}
+
+
