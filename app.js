375d17da5742ec129df0e83183a19921ca4f8790
joninoni <jonibeltranhayer@gmail.com>
2023-12-16 00:36:38 -0600

    funcion que valida el formulario

--- app.js
+++ app.js
@@ -11,5 +11,22 @@ function listaEventos(){
 // Funciones
 function agragarCaducado(e){
     e.preventDefault();
-    console.log("agregando caducado");
+    const producto=document.querySelector("#producto").value;
+    const codigo=document.querySelector("#codigo").value;
+    const cantidad=document.querySelector("#cantidad").value;
+    const fecha=document.querySelector("#fecha").value;
+
+    if(producto==="",codigo==="",cantidad==="",fecha===""){
+        console.log("no puede haber campos vacios");
+        return;
+    }
+    const caducado={
+        producto,
+        codigo,
+        cantidad,
+        fecha,
+    }
+    productosCaducados=[...productosCaducados,caducado];
+    // console.log(productosCaducados);
+    // crearHtml();
 }
